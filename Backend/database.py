569d78704737e6058e0bd0f2b07ea671257a0964
husarixxx <janekdebest21@gmail.com>
2025-07-23 01:00:01 +0200

    Correct database path

--- Backend/database.py
+++ Backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'sqlite:///./login.db'
+DATABASE_URL = 'sqlite:///Flashify/Backend/login.db'
 
 engine  = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
 
