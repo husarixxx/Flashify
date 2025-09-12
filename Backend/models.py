1cd50572bd4ddabfaa73a5fbbf55d96fdd8d03a6
husarixxxx <janekdebest21@gmail.com>
2025-09-12 14:41:34 +0200

    Added model and schemas for quizzes

--- Backend/models.py
+++ Backend/models.py
@@ -1,6 +1,7 @@
 from database import Base
-from sqlalchemy import Column,Integer, String, ForeignKey
+from sqlalchemy import Column,Integer, String, ForeignKey,Boolean
 from sqlalchemy.orm import relationship
+
 
 class Users(Base):
     __tablename__ = 'users'
@@ -11,6 +12,7 @@ class Users(Base):
     hashed_password = Column(String, nullable=False)
 
     flashcards = relationship("Flashcard", back_populates="user", cascade="all, delete-orphan")
+    quiz_sbujects = relationship("QuizSubject", back_populates="user", cascade="all, delete-orphan")
 
 class Flashcard(Base):
     __tablename__ = 'flashcards'
@@ -32,3 +34,48 @@ class Subject(Base):
     name = Column(String, nullable=False, unique=True)
 
     flashcards = relationship("Flashcard", back_populates="subject")
+
+
+
+class QuizSbubject(Base):
+    __tablename__ = "quiz_subjects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    
+    user = relationship("Users", back_populates="quiz_subjects")
+    quizzes = relationship("Quiz",back_populates="subject", cascade="all, delete-orphan")
+
+class Quiz(Base):
+    __tablename__ = "quizzes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    subject_id = Column(Integer, ForeignKey('quiz_subjects.id'), nullable=False)
+
+    subject = relationship("QuizSubject", back_populates="quizzes")
+    questions = relationship("QuizQuestion", back_populates="quiz",cascade="all, delete-orphan")
+
+class QuizQuestion(Base):
+    __tablename__ = "quiz_questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    quiz_id = Column(Integer, ForeignKey('quizzes.id'), nullable=False)
+
+
+    quiz = relationship("Quiz",back_populates="questions",)
+    answers = relationship("QuizAnswer", back_populates="question", cascade="all, delete-orphan")
+
+
+class QuizAnswer(Base):
+    __tablename__ = "quiz_answers"
+
+    id = Column(Integer, primary_key=True,index=True)
+    text = Column(String, nullable=False)
+    is_correct = Column(Boolean, default=False)
+    question_id = Column(Integer, ForeignKey('quiz_questions.id'), nullable=False)
+
+    question = relationship("QuizQuestion", back_populates="answers")          
